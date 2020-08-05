b3844ca9b0b02f7cbc1b2f53719cfa654308f28a
anordy <anordypatrick98@gmail.com>
2020-08-05 12:14:20 +0300

    change table

--- database/migrations/2020_07_21_081157_create__challange_types_table.php
+++ database/migrations/2020_07_21_081157_create__challange_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateChallangeTypeTable extends Migration
+class CreateChallangeTypesTable extends Migration
 {
     /**
      * Run the migrations.
